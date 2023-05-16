c21e48f20b74e54c19a7deb6a5075b136e0b28fd
Bernard Rouvreau <bernard.rouvreau@free.fr>
2023-05-17 01:21:40 +0200

    import_requests.py fonctionnel pour 1 seule page

--- import_requests.py
+++ import_requests.py
@@ -1,18 +1,25 @@
 import requests
 
-from bs4 import BeautifulSoup as bs
+from bs4 import BeautifulSoup
 
-URL = "https://www.gov.uk/search/news-and-communications"
+URL = "https://books.toscrape.com/"
 
-ITEM_DESCRIPTION_CLASS = "gem-c-document-list__item-description"
-TIMEOUT = 15
-TAG = 'p'
+TIMEOUT_REQUEST = 15
 
-page = requests.get(URL, timeout=TIMEOUT)
-soup = bs(page.content, 'html.parser')
-descriptions = soup.find_all(TAG, class_=ITEM_DESCRIPTION_CLASS)
-liste = soup.find_all("p", class_="gem-c-document-list__item-description")
-for ligne in liste:
-    texte = ligne.get_text()
-    print(texte)
-    print("\n")
+page_html = requests.get(URL, timeout=TIMEOUT_REQUEST)
+
+soup = BeautifulSoup(page_html.content, 'html.parser')
+
+# le lien partiel de chaque livre est inclu dans une suite de balises
+# <article class="product_pod"><h"> 20 articles par page
+# Création d'une liste de liens complets pour accéder à l'URL de chaque
+#  livre sur 1 page.
+
+selection_article = soup.select("article.product_pod h3")
+
+liste_liens_par_page = []
+for element in selection_article:
+    lien = element.find("a")
+    link = lien["href"]
+    liste_liens_par_page.append(URL + link)
+print(liste_liens_par_page)
