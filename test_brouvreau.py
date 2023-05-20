c0632dd9c8f86cfe05a155fd42cbea03f65d245c
Bernard Rouvreau <bernard.rouvreau@free.fr>
2023-05-20 17:38:41 +0200

    Validation phase_1.py Phase 1

--- test_brouvreau.py
+++
@@ -1,9 +0,0 @@
-from bs4 import BeautifulSoup
-import requests
-main_url = "https://fr.wikipedia.org/wiki/Hello_world"
-req = requests.get(main_url)
-soup = BeautifulSoup(req.text, "html.parser")
-
-# Finding the main title tag.
-title = soup.find('title')
-print(title.get_text(separator='|'))
